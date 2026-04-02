362878773dbd077cc7a4315e6791ed2f8ea0a86d
ByteEngage <panditharshit1@gmail.com>
2026-04-02 14:00:17 +0530

    test

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState(email);
+  const [password, setPassword] = useState(password);
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
